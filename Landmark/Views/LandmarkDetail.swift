d013cadd9247bdd55081ee3df607419fcdba9eec
久保田聡 <kubota.sou@vega.ocn.ne.jp>
2021-06-20 23:44:29 +0900

    LandmarkDetail,CircleImage,MapViewが変数を受け取れるように変更

--- Landmark/Views/LandmarkDetail.swift
+++ Landmark/Views/LandmarkDetail.swift
@@ -7,19 +7,21 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
         VStack {
-            MapView()
+            MapView(coordinates: landmark.coordinates)
                 .ignoresSafeArea(edges:. top)
                 .frame(height: 300)
             
-            CircleImage()
+            CircleImage(imageName: landmark.imageName)
                 .offset(y: -130)
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text("百年記念館")
+                Text(landmark.name)
                     .font(.title)
                 HStack {
                     Text("東京工業大学")
@@ -44,11 +46,25 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            
-            
-            
+        LandmarkDetail(
+            landmark: Landmark(
+                id: 1001,
+                name: "Turtle Rock",
+                category: "Rivers",
+                city: "Twentynine Palms",
+                state: "California",
+                isFeatured: true,
+                isFavorite: true,
+                park: "Joshua Tree National Park",
+                coordinates: Coordinate(
+                    longitude: -116.166868,
+                    latitude: 34.011286
+                ),
+                description: "Suscipit inceptos ...",
+                imageName: "turtlerock"
+            )
+        )            
     }
 }
